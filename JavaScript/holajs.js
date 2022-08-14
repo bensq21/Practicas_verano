f2df5dec524d9723557bae03719a8fd3d237fc74
Alvaro Vazquez <dalvaro21play@gmail.com>
2022-08-14 23:20:42 +0200

    alert condicionado por el tamaño de 2 palabras en un array

---
+++ JavaScript/holajs.js
@@ -0,0 +1,21 @@
+/*
+var tex = "código";
+tex2 = "messages";
+
+var semana = ["lunes ", "martes ", "miercoles ", "jueves ", "viernes ", "sabadoi ", "domingo"];
+
+alert("Esto es un " + tex + " en un archivo .js de bensq");
+alert("varios " + tex2 + "...");
+
+
+semana.forEach(element => {
+    alert(element);
+});
+*/
+
+valores = [true, 5, false, "hola", "adiosssssssss", 2];
+if (valores[3].length > valores[4].length) {
+    alert(valores[3]);
+} else {
+    alert(valores[4]);
+};
