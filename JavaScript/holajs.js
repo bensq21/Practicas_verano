c9fb2556d7c538bab2c92a3fe122eed92ce9c72c
Alvaro Vazquez <dalvaro21play@gmail.com>
2022-08-15 05:47:36 +0200

    Comprobador de DNI

--- JavaScript/holajs.js
+++ JavaScript/holajs.js
@@ -1,37 +1,21 @@
-/*
-var tex = "código";
-tex2 = "messages";
+var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
 
-var semana = ["lunes ", "martes ", "miercoles ", "jueves ", "viernes ", "sabadoi ", "domingo"];
+var dni = [0, ""];
 
-alert("Esto es un " + tex + " en un archivo .js de bensq");
-alert("varios " + tex2 + "...");
+do {
+    var err = false;
+    dni[0] = prompt("Indique los números del dni");
+    dni[1] = prompt("Indique la letra del dni");
 
+    if ( 0 >= dni[0] || dni[0] > 99999999 || dni[1].length!=1 ) {
+        alert("El formato es incorrecto");
+        err = true;
 
-semana.forEach(element => {
-    alert(element);
-});
+    } else if ( letras[ dni[0] %23 ] == dni[1] ){
+        alert("El dni especificado es correcto");
+    } else{
+        alert("El dni especificado es incorrecto");
+    }
+    
+} while (err);
 
-valores = [true, 5, false, "hola", "adiosssssssss", 2];
-if (valores[3].length > valores[4].length) {
-    alert(valores[3]);
-} else {
-    alert(valores[4]);
-};
-*/
-
-var num1 = 5;
-var num2 = 8;
-
-if(num1 < num2) {
-  alert("numero1 no es mayor que numero2");
-}
-if(0 < num2) {
-  alert("numero2 es positivo");
-}
-if(num1 <= 0) {
-  alert("numero1 es negativo o distinto de cero");
-}
-if(++num1 < num2) {
-  alert("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
-}
