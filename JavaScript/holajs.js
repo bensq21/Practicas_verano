ffca657f156ea08583af416939662f46d23b6c60
Alvaro Vazquez <dalvaro21play@gmail.com>
2022-08-15 10:24:34 +0200

    bucle for para calcular factorial

--- JavaScript/holajs.js
+++ JavaScript/holajs.js
@@ -1,21 +1,7 @@
-var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+var num = prompt("Introduce un numero para factorizar");
 
-var dni = [0, ""];
+for (let index = num-1; index > 0; index--) {
+    num = num * index;
+}
 
-do {
-    var err = false;
-    dni[0] = prompt("Indique los números del dni");
-    dni[1] = prompt("Indique la letra del dni");
-
-    if ( 0 >= dni[0] || dni[0] > 99999999 || dni[1].length!=1 ) {
-        alert("El formato es incorrecto");
-        err = true;
-
-    } else if ( letras[ dni[0] %23 ] == dni[1] ){
-        alert("El dni especificado es correcto");
-    } else{
-        alert("El dni especificado es incorrecto");
-    }
-    
-} while (err);
-
+alert(num);
