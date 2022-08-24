1a34c46fe3b0b7613582cd1fed0e21f7b3e306b6
Alvaro Vazquez <dalvaro21play@gmail.com>
2022-08-24 21:33:16 +0200

    prueba de bucles for each e in con dias de la semana (v.2 sin terminar)

--- JavaScript/holajs.js
+++ JavaScript/holajs.js
@@ -1,16 +1,18 @@
-var dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+var dias = ["        Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
 var res = prompt("Elige each ó in");
 
 if (res.match("each")) {
     alert("Eligió un bucle for each");
     dias.forEach(element => {
-        alert(element + " each");
+        element = element.trim() + " each sum" + 1;
+        alert(element);
     });
 
 } else if(res.match("in")){
     alert("Eligió un bucle for in");
     for (element in dias) {
-        alert(dias[element] + " in");
+        dias[element].concat(" in concatenado");//por algun motivo no consigo que muestre el texto que se está concatenando
+        alert(dias[element]);
 
     }
 } else {
